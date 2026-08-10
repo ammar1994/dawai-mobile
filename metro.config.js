92ece0dc90dd3a1bd85098ad44d3a2aee6e76d03
ammar1994 <awmmar1994@gmail.com>
2026-08-10 11:55:12 +0300

    fix: block react-native-screens src/fabric from Metro to fix RN 0.79 Codegen

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,15 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
-const config = {};
+const config = {
+  resolver: {
+    blockList: [
+      // Exclude duplicate dawai/ folder
+      /dawai\/.*/,
+      // Exclude react-native-screens raw TypeScript fabric sources
+      // (use compiled lib/ instead to avoid Codegen parser errors)
+      /node_modules\/react-native-screens\/src\/fabric\/.*/,
+    ],
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
