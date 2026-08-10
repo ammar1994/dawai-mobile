5d2a584ba019b7cc595cfb0f6acf1ba48d61f3c9
ammar1994 <awmmar1994@gmail.com>
2026-08-10 08:35:38 +0300

    fix: exclude react-native-screens fabric from metro bundler

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,12 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
-const config = {};
+const config = {
+  resolver: {
+    // استبعاد ملفات fabric من react-native-screens لتجنب مشاكل codegen
+    blockList: [
+      /node_modules\/react-native-screens\/src\/fabric\/.*/,
+    ],
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
