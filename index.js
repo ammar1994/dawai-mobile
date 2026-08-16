59540333c6312ae42241c8f72e25c90bce20975c
ammar1994 <awmmar1994@gmail.com>
2026-08-16 16:37:09 +0300

    fix: remove @react-native-firebase from index.js

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 import { AppRegistry, I18nManager } from 'react-native';
-import messaging from '@react-native-firebase/messaging';
 import App from './App';
 import { name as appName } from './app.json';
 
 // ─── RTL دائماً ──────────────────────────────────────────────────────────────
 I18nManager.forceRTL(true);
 I18nManager.allowRTL(true);
-
-// ─── Firebase Background Message Handler ────────────────────────────────────
-messaging().setBackgroundMessageHandler(async remoteMessage => {
-  // يمكن إضافة local notification هنا إذا لزم
-});
 
 AppRegistry.registerComponent(appName, () => App);
