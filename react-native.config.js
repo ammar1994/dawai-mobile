6228028e07c9da596b520d91a1c5751453ce8657
ammar1994 <awmmar1994@gmail.com>
2026-08-16 14:39:39 +0300

    fix: update packageName to com.dawai.pharmacy

---
+++ react-native.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  project: {
+    android: {
+      packageName: 'com.dawai.pharmacy',
+      sourceDir: './android',
+    },
+    ios: {
+      sourceDir: './ios',
+    },
+  },
+};
